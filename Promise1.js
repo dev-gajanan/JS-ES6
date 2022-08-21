be9691e99f79688d660fecc2f0af7678375e348a
Gajanan Mallik <gajanan.mallik@tdcx.com>
2022-08-21 22:56:57 +0530

    promise output question

---
+++ Promise1.js
@@ -0,0 +1,12 @@
+const PromiseMe = () => {
+    console.log(1);
+    new Promise((resolve, reject) => {
+        console.log(2);
+        resolve(3);
+        console.log(4);
+    })
+    .then(resolve => console.log(resolve));
+    console.log(5);
+}
+
+PromiseMe();
